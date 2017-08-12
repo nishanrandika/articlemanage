6339414ad501cc06bc4eff78a4eb2d8c3951ac4a
nishanr <nishanr@craftbyorange.com>
2017-08-13 01:05:29 +0530

    READ route for ALL articles.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -18,7 +18,8 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        return Response::json($articles);
     }
 
     /**
