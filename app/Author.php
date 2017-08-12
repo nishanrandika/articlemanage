cb2ff852a01ece78c67d1d651d8e9dde7e43d07d
nishanr <nishanr@craftbyorange.com>
2017-08-13 00:12:45 +0530

    Migrating author, creates Author model, controller and requests. Author module done.

---
+++ app/Author.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'authors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name'];
+}
---
+++ database/migrations/2017_08_12_175351_create_authors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Authors', function (Blueprint $table) {
+            $table->increments('id')->unique;
+            $table->string('name',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Authors');
+    }
+}
